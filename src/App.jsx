f6ddbe9ad8b54998c46a2d9d895ccbc77b2eb362
yvan-hirwa <hirwakaranganwayvan@gmail.com>
2025-05-11 18:24:49 +0200

    did structure and finished home section

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+import { LoadingScreen } from './components/LoadingScreen'
+import "./index.css"
+import { useEffect } from 'react'
+import Navbar from './components/Navbar'
+import MobileMenu from './components/MobileMenu'
+import Home from './components/sections/Home'
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
+  return (
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div className={`min-h-screen transition-opacity duration-700 ${
+        isLoaded ? 'opacity-100' : 'opacity-0'
+        } bg-black text-gray-100`}
+        >
+          <Navbar menuOpen={menuOpen} setMenuOpen = {setMenuOpen}/>
+          <MobileMenu menuOpen={menuOpen} setMenuOpen = {setMenuOpen}/>
+
+          <Home />
+      </div>
+    </>
+  );
+}
+
+export default App
---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,29 @@
+
+const Home = () => {
+  return (
+    <section id="home" className="min-h-screen flex items-center justify-center relative">
+        <div className="text-center z-10 px-4">
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">Hi, I'm Yvan Hirwa</h1>
+
+            <p className="text-gray-400 text-lg mb-9 max-w-lg mx-auto">
+                I'm a passionate software engineer with a knack for creating innovative solutions. 
+                I specialize in full-stack development, 
+                and I'm always eager to learn and grow in the tech world. 
+                Let's connect and build something amazing together!
+            </p>
+            <div className="flex justify-center space-x-4">
+                <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246, 0.4)]">
+                   View Projects
+                </a>
+
+                <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200
+                 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246, 0.2)] hover:bg-blue-500/10 ">
+                    Contact Me
+                </a>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Home
